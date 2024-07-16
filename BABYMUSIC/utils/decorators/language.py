83631f06d8586e99cbac9cf1142d7c425d25960a
BABY-MUSIC <tridevk442@gmail.com>
2024-07-16 16:53:12 +0530

    Update language.py

--- BABYMUSIC/utils/decorators/language.py
+++ BABYMUSIC/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from SHUKLAMUSICmisc import SUDOERS
-from SHUKLAMUSIC.utils.database import get_lang, is_maintenance
+from BABYMUSICmisc import SUDOERS
+from BABYMUSIC.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
