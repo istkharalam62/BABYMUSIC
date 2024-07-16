5d6ff66fc0a4c26ae62772953ca504b318ade5be
BABY-MUSIC <tridevk442@gmail.com>
2024-07-16 23:31:25 +0530

    Update population.py

--- BABYMUSIC/plugins/Yumi/population.py
+++ BABYMUSIC/plugins/Yumi/population.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
-from SHUKLAMUSIC import app
+from BABYMUSIC import app
 
 
 @app.on_message(filters.command("population"))
