87c67dd520848455e4340121bf860e4bf3710e2b
BABY-MUSIC <tridevk442@gmail.com>
2024-07-17 00:03:03 +0530

    Update stop.py

--- BABYMUSIC/plugins/admins/stop.py
+++ BABYMUSIC/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from BABYMUSIC import app
-from BABYMUSIC.core.call import SHUKLA
+from BABYMUSIC.core.call import BABY
 from BABYMUSIC.utils.database import set_loop
 from BABYMUSIC.utils.decorators import AdminRightsCheck
 from BABYMUSIC.utils.inline import close_markup
