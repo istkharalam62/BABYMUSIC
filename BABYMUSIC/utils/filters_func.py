abcb0999e14996444099a8517653ed16543700e0
BABY-MUSIC <tridevk442@gmail.com>
2024-07-17 06:23:42 +0530

    Update filters_func.py

--- BABYMUSIC/utils/filters_func.py
+++ BABYMUSIC/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
 from SHUKLAMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from SHUKLAMUSIC.utils.msg_types import button_markdown_parser
-from SHUKLAMUSIC.utils.notes_func import NoteFillings
+from BABYMUSIC.utils.msg_types import button_markdown_parser
+from BABYMUSIC.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 
